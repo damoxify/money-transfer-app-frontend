ca13bfc706ac491917eb01fcf4184048be66949e
Adedamola Ajasa <azeemajasa@gmail.com>
2023-12-10 19:08:45 +0100

    feat: Integrate Beneficiaries components into App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-// App.js
-
 import React from 'react';
-import { TransactionProvider } from './context/TransactionContext';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import BeneficiariesList from './components/beneficiaries/BeneficiariesList';
+import AddBeneficiaryForm from './components/beneficiaries/AddBeneficiaryForm';
 import TransactionHistoryPage from './components/transactions/TransactionHistoryPage';
 import Header from './common/Header';
 import Footer from './common/Footer';
 import './styles/global.css'; 
+import { BeneficiariesProvider } from './context/BeneficiariesContext';
+import { TransactionProvider } from './context/TransactionContext';
 
 const App = () => {
   return (
-    <TransactionProvider>
-      <div className="app">
-        <Header />
-        <main className="main-content">
-          {/* Your other components */}
-          <TransactionHistoryPage />
-        </main>
-        <Footer />
-      </div>
-    </TransactionProvider>
+    <Router>
+      <BeneficiariesProvider>
+        <TransactionProvider>
+          <div className="app">
+            <Header />
+            <main className="main-content">
+              <Switch>
+                <Route path="/beneficiaries" component={BeneficiariesList} />
+                <Route path="/add-beneficiary" component={AddBeneficiaryForm} />
+                <Route path="/transactions" component={TransactionHistoryPage} />
+                <Route exact path="/" component={TransactionHistoryPage} />
+              </Switch>
+            </main>
+            <Footer />
+          </div>
+        </TransactionProvider>
+      </BeneficiariesProvider>
+    </Router>
   );
 };
 
