b33c278091aec34f4cbf166501cc5243b627ba92
Adedamola Ajasa <96835056+damoxify@users.noreply.github.com>
2023-12-21 09:01:03 +0100

    feat: Add  Homepage

---
+++ src/components/homepage/ServicesSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ServicesSection = () => {
+  return (
+    <section className="services-section">
+      <h2>Our Services</h2>
+      {/* Add services or components showcasing your app's services */}
+    </section>
+  );
+};
+
+export default ServicesSection;
